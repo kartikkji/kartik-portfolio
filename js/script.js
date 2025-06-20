a202f04027073f8f1911ea054ca9d875dc009848
KARTIK KUMAR <kartikkumarji24@gmail.com>
2025-06-20 23:59:22 +0530

    Update contact form action and add submission alert functionality

--- js/script.js
+++ js/script.js
@@ -15,3 +15,10 @@
       icon.classList.add("fa-sun");
     }
   });
+
+   const form = document.querySelector("form");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        alert("Thank you! Your message has been sent.");
+        form.reset();
+    });
